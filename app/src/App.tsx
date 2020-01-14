69c3ee2d35b89cd689711c591ed975db95b44af7
dmenendez1404 <dmenendez1404@gmail.com>
2020-01-13 19:47:25 -0500

    deploy to Firebase with ci/cd

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -10,13 +10,8 @@ const App: React.FC = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a>
+          Deploy by Travis
         </a>
       </header>
     </div>
