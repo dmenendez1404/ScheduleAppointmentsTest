481696f229efa7f909c29b5417deab4c79553bab
dmenendez1404 <dmenendez1404@gmail.com>
2020-01-29 15:49:11 -0500

    test(custominput.spec): fix Text when is dirty
    Fix Text when is dirty

--- app/src/utils/__tests__/fieldValidators.spec.ts
+++ app/src/utils/__tests__/fieldValidators.spec.ts
@@ -1,6 +1,6 @@
 import { getEmptyControl } from '../ControlHelper';
 import { VALIDATOR_MESSAGES } from '..';
-import { _isEmail, _isRequired, isValidControl, _isEmpty } from '../FieldValidators';
+import { _isRequired, isValidControl, _isEmpty } from '../FieldValidators';
 
 describe('Field Validators', () => {
     it('should get invalid control with no validators', async (done) => {
@@ -26,7 +26,7 @@ describe('Field Validators', () => {
         done();
     });
 
-    
+
     it('should get invalid with empty value on _isEmpty', async (done) => {
         let control = getEmptyControl();
         control = _isEmpty(control);
@@ -35,23 +35,6 @@ describe('Field Validators', () => {
         done();
     });
 
-    it('should get valid with valid email on _isEmail', async (done) => {
-        let control = getEmptyControl();
-        control.value = 'test@gmail.com';
-        control = _isEmail(control);
-        expect(control.error).toEqual(false);
-        done();
-    });
-
-    
-    it('should get invalid with not valid email on _isEmail', async (done) => {
-        let control = getEmptyControl();
-        control.value= 'asd@';
-        control = _isEmail(control);
-        expect(control.error).toEqual(true);
-        expect(control.mensaje).toEqual(VALIDATOR_MESSAGES.Email_wrong_format);
-        done();
-    });
 
     it('should get valid with valid email on _isRequired', async (done) => {
         let control = getEmptyControl();
